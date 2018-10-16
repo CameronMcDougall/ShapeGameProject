1dffeab2969bde7d14bbfce861bf99fd1678c369
Kristian Hansen <hansenkris@ecs.vuw.ac.nz>
2018-10-16 13:31:26 +1300

    Start of water current implementation

---
+++ Assets/_Scripts/WaterCurrent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Script for moving the player with a water current. 
+ * 
+ * Author: Kristian Hansen
+ */
+public class WaterCurrent : MonoBehaviour {
+
+    [SerializeField]
+    public float velocity; // magnitude of speed of objects
+    [SerializeField]
+    public float direction; // angle (0-360) at which the water current moves
+
+	// Use this for initialization
+	void Start () {
+        Mathf.Clamp(direction, 0f, 360f);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+}
